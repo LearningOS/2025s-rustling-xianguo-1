7de1fa0ba00434e6aea7bc5b1e22f1841554b3cd
Xia_1 <1146474373@qq.com>
2025-04-09 21:20:18 +0800

    update

--- exercises/structs/structs1.rs
+++ exercises/structs/structs1.rs
@@ -1,3 +1,49 @@
+/*
+ * @@author        : xia_1  guo_114@outlook.com
+ * @@date          : 2025-03-31 21:05
+ * @@file          : \learnd:\CODE\RUST\CAMP\2025s-rustling-xianguo-1\exercises\structs\structs1.rs
+ * @@name          : 
+ * @@details       : 
+ * @@version       : 1.0
+ * @@par           : null
+ * @@warning       : FBI Warning!
+ * @copyright    : Copyright(c) 2025 by xia_1, All Rights Reserved. 
+ * 
+ * *******************coding=utf-8*******************
+ * *******************鲲鲲保佑无bug*******************
+ * @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+ * @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@%*++#%@@@@@@@@@@@
+ * @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@#=-::::-+%@@@@@@@@@
+ * @@@@@@@@@@@@@@@@@@@@@#*+=-#%+##:...:-::::*@@@@@@@@
+ * @@@@@@@@@@@@@@@@@@@#-.....=%...:::.=*=-:.=@@@@@@@@
+ * @@@@@@@@@@@@@@@@@@+....::..%=.:....:+*+++*@@@@@@@@
+ * @@@@@@@@@@@@@@@@#: ........#* ......:#@@@@@@@@@@@@
+ * @@@@@@@@@@@@@@@=  . .......=#-:......=@@@@@@@@@@@@
+ * @@@@@@@@@@@@@@+ ...+:......-*-.--. .. +@@@@@@@@@@@
+ * @@@@@@@@@@@@@@@- ..:.......#= ..-++-. :@@@@@@@@@@@
+ * @@@@@@@@@@@@@@@@= . ..... -#.... .-**--@@@@@@@@@@@
+ * @@@@@@@@@@@@@@@@@= ...... #+ ..... .-+#@@@@@@@@@@@
+ * @@@@@@@@@@@@@@@@@@*:.... .*. ......  .+@@@@@@@@@@@
+ * @@@@@@@@@@@@@@@@*+*++=--:-=........  =%@@@@@@@@@@@
+ * @@@@@@@@@@@@@@@%=++++========--::.  -@@@@@@@@@@@@@
+ * @@@@@@@@@@@@@%#+++++=============-+%@@@@@@@@@@@@@@
+ * @@@@@@@@@@@%*+++++================+*@@@@@@@@@@@@@@
+ * @@@@@@@@@%*+++++===-------------====@@@@@@@@@@@@@@
+ * @@@@@@@@#+======--=+*#%%%%#+=---====*@@@@@@@@@@@@@
+ * @@@@@@@*+===--=+#%@@@@@@@@@@@*--=====*@@@@@@@@@@@@
+ * @@@@@@#++=--*#@@@@@@@@@@@@@@@@%+-----=#@@@@@@@@@@@
+ * @@@@@@*+++=#@@@@@@@@@@@@@@@@@@@@#+--=++%@@@@@@@@@@
+ * @@@@@%+++==@@@@@@@@@@@@@@@@@@@@@@@%++++*@@@@@@@@@@
+ * @@@@@%+++=*@@@@@@@@@@@@@@@@@@@@@@@@*=+++%@@@@@@@@@
+ * @@@@@#++=+@@@@@@@@@@@@@@@@@@@@@@@@@%=+++*@@@@@@@@@
+ * @@@@@#++=%@@@@@@@@@@@@@@@@@@@@@@@@@@#=+++%@@@@@@@@
+ * @@@@@+=-*@@@@@@@@@@@@@@@@@@@@@@@@@@@@#=++%@@@@@@@@
+ * @@@@%:::@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@+::+@@@@@@@@
+ * @@@@=.-:*@@@@@@@@@@@@@@@@@@@@@@@@@@@@@%:-:*@@@@@@@
+ * @@%+::::-@@@@@@@@@@@@@@@@@@@@@@@@@@@@@%--::*@@@@@@
+ */
+
+
 // structs1.rs
 //
 // Address all the TODOs to make the tests pass!
@@ -5,13 +51,16 @@
 // Execute `rustlings hint structs1` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
+
 
 struct ColorClassicStruct {
     // TODO: Something goes here
-}
+    red: i32,
+    green: i32,
+    blue: i32,
+}   
 
-struct ColorTupleStruct(/* TODO: Something goes here */);
+struct ColorTupleStruct( i32,i32,i32/* TODO: Something goes here */);
 
 #[derive(Debug)]
 struct UnitLikeStruct;
@@ -24,6 +73,11 @@ mod tests {
     fn classic_c_structs() {
         // TODO: Instantiate a classic c struct!
         // let green =
+        let green = ColorClassicStruct {
+            red: 0,
+            green: 255,
+            blue: 0,
+        };
 
         assert_eq!(green.red, 0);
         assert_eq!(green.green, 255);
@@ -34,6 +88,7 @@ mod tests {
     fn tuple_structs() {
         // TODO: Instantiate a tuple struct!
         // let green =
+        let green = ColorTupleStruct(0, 255, 0);
 
         assert_eq!(green.0, 0);
         assert_eq!(green.1, 255);
@@ -44,6 +99,7 @@ mod tests {
     fn unit_structs() {
         // TODO: Instantiate a unit-like struct!
         // let unit_like_struct =
+        let unit_like_struct = UnitLikeStruct;
         let message = format!("{:?}s are fun!", unit_like_struct);
 
         assert_eq!(message, "UnitLikeStructs are fun!");
